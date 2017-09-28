8f7b4822d460aea6da1aad8338bacea176d9b970
sejonghumble <30329794+sejonghumble@users.noreply.github.com>
2017-09-28 10:16:01 +0900

    스마트 에디터 추가 title,content 로 구성됨 content는 title제외 모든 정보들어감 얻어올때도 content로 얻어오면 됨

---
+++ newface2/src/main/java/com/newface/controller/SmartController.java
@@ -0,0 +1,148 @@
+package com.newface.controller;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.UUID;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.context.ServletContextAware;
+import org.springframework.web.servlet.ModelAndView;
+
+public class SmartController implements ServletContextAware {
+	private ServletContext context;
+	
+	@RequestMapping(value = "/file_uploader_html5", method = RequestMethod.POST)
+	public void file_uploader_html5(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		String sFileInfo = "";
+		//파일명 - 싱글파일업로드와 다르게 멀티파일업로드는 HEADER로 넘어옴 
+		String name = request.getHeader("file-name");
+		String ext = name.substring(name.lastIndexOf(".")+1);
+		System.out.println("name : " + name);
+		System.out.println("ext : " + ext);
+
+		//파일 기본경로
+		String root = request.getContextPath() + "/resources";
+		String defaultPath = context.getRealPath("/resources");
+		System.out.println("root : " + root);
+		//파일 기본경로 _ 상세경로
+		String path = defaultPath + File.separator + "upload" + File.separator;
+		System.out.println("path : " + path);
+		File file = new File(path);
+		if(!file.exists()) {
+		  file.mkdirs();
+		}
+		String realname = UUID.randomUUID().toString() + "." + ext;
+		InputStream is = request.getInputStream();
+		String storedFileName = path + realname;
+		System.out.println("storedFileName : " + storedFileName);
+		OutputStream os=new FileOutputStream(storedFileName);
+		int numRead;
+		//파일쓰기
+		byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
+		while((numRead = is.read(b,0,b.length)) != -1){
+		  os.write(b,0,numRead);
+		}
+		if(is != null) {
+		  is.close();
+		}
+		os.flush();
+		os.close();
+
+		sFileInfo += "&bNewLine=true&sFileName="+ name+"&sFileURL=" + root + "/upload/"+realname;
+		PrintWriter out = response.getWriter();
+		System.out.println(sFileInfo);
+		out.println(sFileInfo);
+	}
+	
+	@RequestMapping(value="/file_uploader", method=RequestMethod.POST)
+	public void file_uploader(HttpServletRequest request, HttpServletResponse response) throws IOException, FileUploadException {
+		request.setCharacterEncoding("utf-8");
+		String return1="";
+		String return2="";
+		String return3="";
+		String name = "";
+		if (ServletFileUpload.isMultipartContent(request)){
+		    ServletFileUpload uploadHandler = new ServletFileUpload(new DiskFileItemFactory());
+		    //UTF-8 인코딩 설정
+		    uploadHandler.setHeaderEncoding("UTF-8");
+		    List<FileItem> items = uploadHandler.parseRequest(request);
+		    //각 필드태그들을 FOR문을 이용하여 비교를 합니다.
+		    for (FileItem item : items) {
+		        if(item.getFieldName().equals("callback")) {
+		            return1 = item.getString("UTF-8");
+		        } else if(item.getFieldName().equals("callback_func")) {
+		            return2 = "?callback_func="+item.getString("UTF-8");
+		        } else if(item.getFieldName().equals("Filedata")) {
+		            //FILE 태그가 1개이상일 경우
+		            if(item.getSize() > 0) {
+		            	name = item.getName();
+		                String ext = item.getName().substring(item.getName().lastIndexOf(".")+1);
+		                //파일 기본경로
+		                String defaultPath = context.getRealPath("/");
+		                //파일 기본경로 _ 상세경로
+		                String path = defaultPath + "upload" + File.separator;
+		                 
+		                File file = new File(path);
+		                 
+		                //디렉토리 존재하지 않을경우 디렉토리 생성
+		                if(!file.exists()) {
+		                    file.mkdirs();
+		                }
+		                //서버에 업로드 할 파일명(한글문제로 인해 원본파일은 올리지 않는것이 좋음)
+		                String realname = UUID.randomUUID().toString() + "." + ext;
+		                ///////////////// 서버에 파일쓰기 ///////////////// 
+		                InputStream is = item.getInputStream();
+		                OutputStream os=new FileOutputStream(path + realname);
+		                int numRead;
+		                byte b[] = new byte[(int)item.getSize()];
+		                while((numRead = is.read(b,0,b.length)) != -1){
+		                    os.write(b,0,numRead);
+		                }
+		                if(is != null)  is.close();
+		                os.flush();
+		                os.close();
+		                ///////////////// 서버에 파일쓰기 /////////////////
+		                String root = request.getContextPath();
+
+		                return3 += "&bNewLine=true&sFileName="+name+"&sFileURL=" + root + "/upload/"+realname;	// by ksseo
+		            }else {
+		                return3 += "&errstr=error";
+		            }
+		        }
+		    }
+		}
+		response.sendRedirect(return1+return2+return3);
+	}
+	
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public ModelAndView write(HttpServletRequest request) throws IOException, FileUploadException {
+		String title = request.getParameter("title");
+		String smarteditor = request.getParameter("smarteditor");
+//		
+//		System.out.println("title = " + title);
+//		System.out.println("content = " + smarteditor);
+		ModelAndView model = new ModelAndView("write");
+		model.addObject("title", title);
+		model.addObject("smarteditor", smarteditor);
+		return model;
+	}
+	
+	@Override
+	public void setServletContext(ServletContext servletContext) {
+		this.context = servletContext;
+	}
+}
--- newface2/src/main/webapp/WEB-INF/views/minihome/diary/diary_update.jsp
+++ newface2/src/main/webapp/WEB-INF/views/minihome/diary/diary_update.jsp
@@ -1,24 +1,35 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>   
-<link rel="stylesheet" type="text/css" href="<c:url value='/resources/css/minihome/diary/diary_folder.css'/>"> 
-<script type="text/javascript" src='<c:url value="/resources/js/jquery-3.2.1.min.js" />'></script>
-<script>
-	$(function(){
-		var str=$("#select").val();
-		$("#type").val(str).prop("selected",true);
-	});
-</script>
-	<input type="hidden" id="select" value="${requestScope.vo.type }">
-<form method="post"  action="<c:url value='/diary/update'/>">
-	<input type="hidden" name="diary_num" value="${requestScope.vo.diary_num }">
-	제목 <input type="text" name="title" value="${requestScope.vo.title }"><br>
-	내용 <textarea rows="5" cols="30" name="content">${requestScope.vo.content }</textarea><br>
-	공개설정 
-	<select name="type" id="type">
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@ page session="false" %>
+	<script type="text/javascript" src="<c:url value="/resources/js/jquery-3.2.1.min.js"/>"></script>
+	<script type="text/javascript" src="<c:url value="/resources/smarteditor/js/HuskyEZCreator.js"/>" charset="utf-8"></script>
+	<script type="text/javascript" charset="utf-8" src="<c:url value="/resources/js/writeForm.js"/>"></script>
+	<link rel="stylesheet" href="<c:url value="/resources/css/common.css"/>"/>
+	<script type="text/javascript" charset="utf-8">
+		sessionStorage.setItem("contextpath", "${pageContext.request.contextPath}");
+	</script>
+	<script>
+		$(function(){
+			var str=$("#select").val();
+			$("#type").val(str).prop("selected",true);
+		});
+	</script>
+<input type="hidden" id="select" value="${requestScope.vo.type }">
+<form action="<c:url value='/diary/update'/>" method="post" id="frm">
+<input type="hidden" name="diary_num" value="${requestScope.vo.diary_num }">
+	<table>
+		<tr>
+			<td>제목</td>
+			<td><input type="text" name="title" id="title" placeholder="제목" value="${requestScope.vo.title }"></td>
+		</tr>
+		<tr>
+			<td style="margin:0; padding:0;" colspan="2"><textarea name="content" id="smarteditor" rows="10" cols="100" style="width:100%; height:412px;">${requestScope.vo.content }</textarea></td>
+		</tr>
+	</table>
+	<select name="type">
 		<option value="전체공개">전체공개</option>
 		<option value="1촌공개">1촌공개</option>
 		<option value="비공개">비공개</option>
-	</select><br>
-	<input type="submit" value="수정">
+	</select>
+	<input id="addBtn" type="submit" value="전송">
 </form>
