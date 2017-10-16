fc343e7833c5323a9e72561aec26a3699e0084df
MourinhoU <idjsmtown@daum.net>
2017-10-16 16:04:38 +0900

    성민 - 쪽지 mapper,service,dao

--- newface2/src/main/java/com/newface/dao/MsgDao.java
+++ newface2/src/main/java/com/newface/dao/MsgDao.java
@@ -1,14 +1,51 @@
 package com.newface.dao;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.newface.vo.MsgVo;
 
 @Repository
 public class MsgDao {
 	@Autowired private SqlSession sqlSession;
 	private final String NAMESPACE="com.newface.mybatis.MsgMapper";
 	
+	public int msg_insert(MsgVo vo) {
+		return sqlSession.insert(NAMESPACE+".msg_insert",vo);
+	}
 	
+	public List<MsgVo> msgrecv_list(HashMap<String, Object> map){
+		return sqlSession.selectList(NAMESPACE+".msgrecv_list",map);
+	}
+	
+	public int msgrecv_count(String receiver) {
+		return sqlSession.selectOne(NAMESPACE+".msgrecv_count",receiver);
+	}
+	
+	public List<MsgVo> msgsend_list(HashMap<String, Object> map){
+		return sqlSession.selectList(NAMESPACE+".msgsend_list",map);
+	}
+	public int msgsend_count(String sender) {
+		return sqlSession.selectOne(NAMESPACE+".msgsend_count",sender);
+	}
+	
+	public int msgrecv_delete(int msg_num) {
+		return sqlSession.update(NAMESPACE+".msgrecv_delete",msg_num);
+	}
+	
+	public int msgsend_delete(int msg_num) {
+		return sqlSession.update(NAMESPACE+".msgsend_delete",msg_num);
+	}
+	
+	public int msgrecv_chk(int msg_num) {
+		return sqlSession.update(NAMESPACE+".msgrecv_chk",msg_num);
+	}
+	public MsgVo msgrecv_getinfo(int msg_num) {
+		return sqlSession.selectOne(NAMESPACE+".msgrecv_getinfo",msg_num);
+	}
 
 }
--- newface2/src/main/java/com/newface/service/MsgService.java
+++ newface2/src/main/java/com/newface/service/MsgService.java
@@ -1,12 +1,55 @@
 package com.newface.service;
 
+import java.util.HashMap;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.newface.dao.MsgDao;
+import com.newface.vo.MsgVo;
 
 @Service
 public class MsgService {
 	@Autowired MsgDao dao;
+	
+	public void setDao(MsgDao dao) {
+		this.dao = dao;
+	}
+	
+	public int msg_insert(MsgVo vo) {
+		return dao.msg_insert(vo);
+	}
+	
+	public List<MsgVo> msgrecv_list(HashMap<String, Object> map){
+		return dao.msgrecv_list(map);
+	}
+	
+	public int msgrecv_count(String receiver) {
+		return dao.msgrecv_count(receiver);
+	}
+	public List<MsgVo> msgsend_list(HashMap<String, Object> map){
+		return dao.msgsend_list(map);
+	}
+	
+	public int msgsend_count(String sender) {
+		return dao.msgsend_count(sender);
+	}
+	
+	public int msgrecv_delete(int msg_num) {
+		return dao.msgrecv_delete(msg_num);
+	}
+	
+	public int msgsend_delete(int msg_num) {
+		return dao.msgsend_delete(msg_num);		
+	}
+	
+	public int msgrecv_chk(int msg_num) {
+		return dao.msgrecv_chk(msg_num);
+	}
+	
+	public MsgVo msgrecv_getinfo(int msg_num) {
+		return dao.msgrecv_getinfo(msg_num);
+	}
 	
 }
