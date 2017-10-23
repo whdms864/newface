7dc0c72fcf65d99522aa03de2a04206e979a7676
JHTA <JHTA@JHTA-PC>
2017-10-23 12:08:05 +0900

    사진첩 수정

--- newface2/src/main/java/com/newface/controller/ManagerController.java
+++ newface2/src/main/java/com/newface/controller/ManagerController.java
@@ -9,16 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ManagerController {
 	
-	@RequestMapping(value = "/adadmin", method = RequestMethod.GET)
-	public String ad() {
-		return ".adadmin";
-	}
-
-	
-	@RequestMapping(value = "/singoadmin", method = RequestMethod.GET)
-	public String singo() {
-		return ".singoadmin";
-	}
 	
 	
 }
